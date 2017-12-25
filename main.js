0cfcc08566f60d2ea8da83482bfde1e87e0c6a4b
Silber <silber.ypm@gmail.com>
2017-12-25 19:40:20 +0500

    Adding .active to CSS

--- main.js
+++ main.js
@@ -1,3 +1,55 @@
 window.onload = function() {
-    alert('document loaded');
+    // alert('document loaded');
+
+    window.onkeydown = function(event) {
+        switch (event.keyCode) {
+            case 37: console.log('Left');
+                break;
+            case 38: console.log('Up');
+                break;
+            case 39: console.log('Right');
+                break;
+            case 40: console.log('Down');
+                break;
+            default: console.log('another keyCode');
+                break;
+        }
+    }
+
+    console.log('document.childNodes'); //all childrens with doctype
+    console.log(document.childNodes);
+    console.log('document.children');   //all childrens
+    console.log(document.children); 
+    console.log('document.children[0].children[0].children (HEAD childrens)');
+    console.log(document.children[0].children[0].children);
+
+console.log(`------------------------------------------`);
+console.log(`document.getElementById('container')`);
+console.log(document.getElementById('container'));
+
+console.log(`------------------------------------------`);
+var divs = window.document.getElementsByTagName('div');
+console.log(divs);
+var container3 = document.getElementById('container3');
+var innerContainer3 = document.createElement("div");
+container3.appendChild(innerContainer3);
+console.log(`innerContainer3`);
+console.log(innerContainer3);
+// innerContainer3.style.width = "10px";
+// innerContainer3.style.height = "10px";
+// innerContainer3.style.border = "1px solid black";
+
+for(var i = 0; i < 8; i++) {
+    container3.innerHTML = '<div style="width: 10px; height: 10px; border: 1px solid black;"></div><div style="width: 10px; height: 10px; border: 1px solid black;"></div><div style="width: 10px; height: 10px; border: 1px solid black;"></div><div style="width: 10px; height: 10px; border: 1px solid black;"></div>';
 }
+
+var container0 = document.getElementById('container0');
+container0.style.fontSize = '2em';
+
+
+
+
+
+}
+
+
